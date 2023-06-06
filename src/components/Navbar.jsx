d7ac03cc0e791b8817aff68187ad13f7f35dce44
Maia Dutta <mermaia33@gmail.com>
2023-06-06 18:54:57 -0500

    skills, about, home, contact and work components created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Logo from '../assets/logo.png'
+import {Link} from 'react-scroll'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -15,11 +16,31 @@ const Navbar = () => {
       {/* Menu */}
       <div>
         <ul className="hidden md:flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
 
@@ -37,11 +58,41 @@ const Navbar = () => {
         }
       >
         <ul>
-          <li className="py-6 text-4xl">Skills</li>
-          <li className="py-6 text-4xl">Work</li>
-          <li className="py-6 text-4xl">Contact</li>
-          <li className="py-6 text-4xl">Home</li>
-          <li className="py-6 text-4xl">About</li>
+          <li className="py-6 text-4xl">
+            <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="skills"
+              smooth={true}
+              duration={500}
+            >
+              Skills
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="contact"
+              smooth={true}
+              duration={500}
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,25 +3,61 @@ import JavaScript from '../assets/javascript.png'
 import ReactImg from "../assets/react.png";
 import Tailwind from "../assets/tailwind.png";
 import GitHub from "../assets/github.png";
+import CSS from "../assets/css.png";
+
 
 const Skills = () => {
   return (
-    <div name='skills'>
-    {/* Container */}
-    <div>
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300 ">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-            <p>Experience</p>
-            <p>//These are the technologies I've worked with</p>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
+            Skills
+          </p>
+          <p className="py-4">// These are the technologies I've worked with</p>
         </div>
-        <div>
-            <div>
-                <img src={JavaScript} alt='JavaScript Icon' />
-                <p>JavaScript</p>
-            </div>
+        <div className="w-full grid grid-cols-4 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src={JavaScript}
+              alt="JavaScript Icon"
+            />
+            <p className="my-4">JavaScript</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React Icon" />
+            <p className="my-4">React</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind Icon" />
+            <p className="my-4">Tailwind</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+            <img className="w-20 mx-auto" src={CSS} alt="CSS Icon" />
+            <p className="my-4">CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+            <img className="w-20 mx-auto" src={GitHub} alt="Github Icon" />
+            <p className="my-4">Github</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="Python Icon" />
+            <p className="my-4">Python</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="Python Icon" />
+            <p className="my-4">Django</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="Python Icon" />
+            <p className="my-4">SQL</p>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
 export default Skills
